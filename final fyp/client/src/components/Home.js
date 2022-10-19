17a90614ee6a383ed459b9139a1a0c4a9b7da79b
64545waqas <waqasmanzoor30fly@gmail.com>
2022-10-20 02:38:52 +0500

    k

--- final fyp/client/src/components/Home.js
+++ final fyp/client/src/components/Home.js
@@ -1,14 +1,82 @@
-import React from "react";
+// import React from "react";
 import './App.css';
-const Home = () => {
-  // return<p>inside Home component</p>;
+import { dataDigitalBestSeller } from './data';
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import imgGirl from './assets/images/Banner1.jpg';
+function Home() {
+  const [Banner1, setBanner1] = useState({});
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const handleErrorImage = (data) => {
+    setBanner1((prev) => ({
+      ...prev,
+      [data.target.alt]: data.target.alt,
+      linkDefault: imgGirl,
+    }));
+  };
 
   return (
-    <>
-    <h1>Home</h1>
+    <div className="App">
+      <Slider {...settings}>
+        {dataDigitalBestSeller.map((item) => (
+          <div className="card">
+            <div className="card-top">
+              <img
+                src={
+                  Banner1[item.title] === item.title
+                    ? Banner1.linkDefault
+                    : item.linkImg
+                }
+                alt={item.title}
+                onError={handleErrorImage}
+              />
+              <h1>{item.title}</h1>
+            </div>
+            <div className="card-bottom">
+              <h3>{item.price}</h3>
+              <span className="category">{item.category}</span>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
 
-    </>
-  )
-
-};
 export default Home;
